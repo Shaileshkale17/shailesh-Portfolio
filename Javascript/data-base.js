eee940e577408b0c9ba188838091e9445d143ef4
Shailesh kale <shaileshkale87730@gmail.com>
2023-12-29 12:53:08 +0530

    remove gitignore to sending data file

--- Javascript/data-base.js
+++ Javascript/data-base.js
@@ -1,45 +1,20 @@
 const form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  let usernameInput = document.querySelector('input[name="name"]');
-  let usernameErr = document.getElementById("Error");
-  
-  if (usernameInput || usernameErr) {
-    // Check if either element is missing in the DOM
-    console.log("Username input or error element not found");
-    return;
-  }
-  
-  if (usernameInput.value.trim() === "") {
-    // Check if the value is empty
-    console.log("Please enter a username");
-    usernameErr.classList.add("error");
-    usernameErr.innerHTML = "Please enter a username";
-    return; // Stop further execution if validation fails
-  }
-  
-  // If validation passes, continue with other actions
-  // ...
-  
-
-
-
   let data = new FormData(form);
   fetch(
-    "https://script.google.com/macros/s/AKfycbxZCcyUOW2xDtq2lnu-pvUxsKr8cTwgXU3p17y_Nx6jc4kJZ-oe5m27EgJGHgmxPs9fag/exec",
+    "https://script.google.com/macros/s/AKfycbx5dXNH2mfADDqiD9YDKkugwb_HBxdOmcVw6pON2tzb1ma8nrmJpZlhv12zFHw_QNFJNA/exec",
     {
       method: "POST",
       body: data,
     }
   )
     .then((response) => response.text())
-    .then((data) => submitData(data));
+    .then((data) => console.log(data));
+
+  resetbutton();
 });
 
-function submitData(data) {
-  if (data === "Success") {
-    console.log("sjdfjs");
-  }
+function resetbutton() {
+  form.reset();
 }
-
-// console.log("objectErr",validateForm());
